5aa7b1571afd1408338af965a71b1b785d2bb2cb
supiash1 <supiash@gmail.com>
2024-06-25 20:25:55 +0100

    Enhance seeds.rb to ensure idempotent setup of users, statuses, and projects with comments

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,3 +10,30 @@
 %w[Unstarted Started Finished].each do |status_name|
   Status.find_or_create_by(name: status_name)
 end
+
+users_attributes = [
+  { email: 'alice@example.com',  password: 'password123' },
+  { email: 'bob@example.com',  password: 'password123' }
+]
+
+users_attributes.each do |user_attr|
+  user = User.find_or_create_by(email: user_attr[:email])
+  user.assign_attributes(user_attr)
+  user.save
+end
+
+status_names = %w[Unstarted Started Finished]
+statuses = status_names.map do |name|
+  Status.find_or_create_by(name:)
+end
+
+projects_attributes = [
+  { name: 'Project 1', status: statuses[0], comments_attributes: [{ body: 'First comment on Project 1' }] },
+  { name: 'Project 2', status: statuses[1], comments_attributes: [{ body: 'First comment on Project 2' }] }
+]
+
+projects_attributes.each do |proj_attr|
+  project = Project.find_or_create_by(name: proj_attr[:name])
+  project.assign_attributes(proj_attr.except(:name))
+  project.save
+end
