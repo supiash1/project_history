e1f15bf3669af951fd119ba7a9c2613014033dbe
supiash1 <supiash@gmail.com>
2024-06-25 20:10:06 +0100

    Generate comment model with body as text and references project

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,4 +1,6 @@
 class Project < ApplicationRecord
   belongs_to :status
   validates_presence_of :name
+  has_many :comments, dependent: :destroy
+  accepts_nested_attributes_for :comments, allow_destroy: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_06_25_185431) do
+ActiveRecord::Schema[7.1].define(version: 2024_06_25_190311) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.text "body"
+    t.bigint "project_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["project_id"], name: "index_comments_on_project_id"
+  end
 
   create_table "projects", force: :cascade do |t|
     t.string "name"
@@ -40,5 +48,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_06_25_185431) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "comments", "projects"
   add_foreign_key "projects", "statuses"
 end
